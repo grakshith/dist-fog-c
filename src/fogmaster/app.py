059ba91a129aa30c14dc9173f2ea8893e837c4be
Rakshith <hehaichi@gmail.com>
2018-03-09 20:27:33 +0530

    Local utilization

--- src/fogmaster/app.py
+++ src/fogmaster/app.py
@@ -1,10 +1,56 @@
 from flask import Flask
 app = Flask(__name__)
 
+# Imports
+import psutil
+from flask import jsonify
+
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-if __name__=='__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
 
+@app.route('/utilization')
+def get_util():
+    cpu_count = psutil.cpu_count()
+    cpu_freq = {
+        'current': psutil.cpu_freq().current,
+        'max': psutil.cpu_freq().max,
+        'min': psutil.cpu_freq().min
+    }
+    cpu_percent = psutil.cpu_percent()
+    disk_util = {
+        'total': psutil.disk_usage('/').total,
+        'used': psutil.disk_usage('/').used,
+        'free': psutil.disk_usage('/').free,
+        'percent': psutil.disk_usage('/').percent
+    }
+    temperatures = psutil.sensors_temperatures()
+    swap_mem = {
+        'total': psutil.swap_memory().total,
+        'used': psutil.swap_memory().used,
+        'free': psutil.swap_memory().free,
+        'percent': psutil.swap_memory().percent
+    }
+    memory = {
+        'total': psutil.virtual_memory().total,
+        'available': psutil.virtual_memory().available,
+        'percent': psutil.virtual_memory().percent,
+        'used': psutil.virtual_memory().used,
+        'free': psutil.virtual_memory().free
+    }
+    utilization = {
+        'cpu_count': cpu_count,
+        'cpu_freq': cpu_freq,
+        'cpu_percent': cpu_percent,
+        'disk_util': disk_util,
+        'temperatures': temperatures,
+        'swap_memory': swap_mem,
+        'memory': memory
+    }
+    return jsonify(utilization)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
